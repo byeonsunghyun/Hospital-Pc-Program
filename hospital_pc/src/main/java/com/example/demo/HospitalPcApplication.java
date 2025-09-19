32a558fd9b1aafc5cb3e9977f85d0cb6daef9e69
pnudh <pnudh@change_on_install>
2025-09-19 15:21:13 +0900

    first test

---
+++ hospital_pc/src/main/java/com/example/demo/HospitalPcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalPcApplication.class, args);
+	}
+
+}
